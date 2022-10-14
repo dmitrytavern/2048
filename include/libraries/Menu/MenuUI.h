21c56e1d3ce290ec26b51bafc6db51df9e5c9f18
Dmitry Tavern <dogger.work@gmail.com>
2022-10-14 03:25:01 +0300

    refactor: improve MenuUI class

--- include/libraries/Menu/MenuUI.h
+++ include/libraries/Menu/MenuUI.h
@@ -7,6 +7,12 @@ using namespace std;
 class MenuUI
 {
 public:
+  static int BORDER_LENGTH;
+  static int BORDER_WINDOW_PADDING;
+
   static void PrintMenu(Menu *menu);
   static void ActivateMenu(Menu *menu);
+  static int CalcContentSize(Menu *menu);
+  static int CalcWindowsSize(int content_size);
+  static int CalcSpacesCount(int windows_size);
 };
--- src/libraries/Menu/MenuUI.cpp
+++ src/libraries/Menu/MenuUI.cpp
@@ -7,45 +7,68 @@
 #include "libraries/Menu/MenuUI.h"
 #include "libraries/UI/UI.h"
 
+int MenuUI::BORDER_LENGTH = 1;
+int MenuUI::BORDER_WINDOW_PADDING = 3;
+
 void MenuUI::PrintMenu(Menu *menu)
 {
-  vector<MenuAction> actions = menu->GetActions();
-  int actions_length = menu->GetActionsLength();
-  int centered_chars_count = UI::GetTerminalWidth() / 2;
-  int window_length = 0;
-  int content_length = 0;
-  int padding_length = 3;
-  int border_length = 1;
+  string menu_title = menu->GetTitle();
+  vector<MenuAction> menu_actions = menu->GetActions();
+  int menu_actions_length = menu->GetActionsLength();
 
-  for (int index = 0; index < actions_length; index++)
+  int content_size = MenuUI::CalcContentSize(menu);
+  int windows_size = MenuUI::CalcWindowsSize(content_size);
+  int spaces_count = MenuUI::CalcSpacesCount(windows_size);
+
+  UI::PrintSpaces(spaces_count);
+  UI::PrintWindowBorderTopWithTitle(windows_size, menu_title);
+
+  for (int index = 0; index < menu_actions_length; index++)
   {
-    int length = actions[index].GetTitle().length();
-    content_length = length > content_length ? length : content_length;
-  }
-
-  window_length += content_length + padding_length * 2 + border_length * 2;
-  centered_chars_count -= window_length / 2;
-
-  UI::PrintSpaces(centered_chars_count);
-  UI::PrintWindowBorderTopWithTitle(window_length, menu->GetTitle());
-
-  for (int index = 0; index < actions_length; index++)
-    if (actions[index].GetTitle() != "")
+    string action_title = menu_actions[index].GetTitle();
+    if (action_title != "")
     {
-      UI::PrintSpaces(centered_chars_count);
+      UI::PrintSpaces(spaces_count);
       cout << UI::BORDER_VERTICAL_CHAR;
-      UI::PrintSpaces(padding_length);
-      cout << std::left << setw(content_length) << actions[index].GetTitle();
-      UI::PrintSpaces(padding_length);
+      UI::PrintSpaces(MenuUI::BORDER_WINDOW_PADDING);
+      cout << std::left << setw(content_size) << action_title;
+      UI::PrintSpaces(MenuUI::BORDER_WINDOW_PADDING);
       cout << UI::BORDER_VERTICAL_CHAR << endl;
     }
+  }
 
-  UI::PrintSpaces(centered_chars_count);
-  UI::PrintWindowBorderBottom(window_length);
+  UI::PrintSpaces(spaces_count);
+  UI::PrintWindowBorderBottom(windows_size);
 }
 
 void MenuUI::ActivateMenu(Menu *menu)
 {
   int pressed_key = getch();
   menu->Trigger(pressed_key);
+}
+
+int MenuUI::CalcContentSize(Menu *menu)
+{
+  vector<MenuAction> menu_actions = menu->GetActions();
+  int menu_actions_length = menu->GetActionsLength();
+
+  int max_content_length = 0;
+
+  for (int index = 0; index < menu_actions_length; index++)
+  {
+    int length = menu_actions[index].GetTitle().length();
+    max_content_length = length > max_content_length ? length : max_content_length;
+  }
+
+  return max_content_length;
+}
+
+int MenuUI::CalcWindowsSize(int content_size)
+{
+  return content_size + MenuUI::BORDER_WINDOW_PADDING * 2 + MenuUI::BORDER_LENGTH * 2;
+}
+
+int MenuUI::CalcSpacesCount(int windows_size)
+{
+  return UI::GetTerminalWidth() / 2 - windows_size / 2;
 }
