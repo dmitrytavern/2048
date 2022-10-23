d9e2cbda0f4e70b1ad2da74113ed9cda6c159673
Dmitry Tavern <dogger.work@gmail.com>
2022-10-24 00:39:35 +0300

    fix(Interface): replace coniolinux
    In debug preset, coniolinux works great, but in the release, it is not working. I replace this lib on the custom function

--- src/interface/DialogInterface.cpp
+++ src/interface/DialogInterface.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "interface/DialogInterface.h"
 #ifdef __linux__
-#include <coniolinux.h>
+#include <unistd.h>
+#include <termios.h>
+int getch()
+{
+  char buf = 0;
+  struct termios old = {0};
+  if (tcgetattr(0, &old) < 0)
+    perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0)
+    perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0)
+    perror("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0)
+    perror("tcsetattr ~ICANON");
+  return (int(buf));
+}
 #else
 #include <conio.h>
 #endif
